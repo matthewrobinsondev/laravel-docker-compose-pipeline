8056dcbaea8e90a750511e60aca898af023f8899
Matt Robinson <matthewrobinsondev@gmail.com>
2023-05-21 20:57:39 +0100

    PSR12 Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-         \App\Models\User::factory()->create([
-             'name' => 'Test User',
-             'email' => 'test@test.com',
-             'password' => 'test'
-         ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@test.com',
+            'password' => 'test',
+        ]);
     }
 }
